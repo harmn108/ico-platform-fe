68470e9ade1306224abcfb099b6dd5d0b05d3e25
Hayk-Grigoryan <haykgrigoryan1989@gmail.com>
2018-02-13 23:11:38 +0400

    marge with styles

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,4 @@
 import {NgModule} from '@angular/core';
-import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
 import {SharedModule} from '../shared/shared.module';
 import {AppRoutingModule} from '../app-routing.module';
@@ -11,14 +10,10 @@ import {
   MatSelectModule, MatCardModule, MatFormFieldModule, MatIconModule, MatDatepickerModule, MatNativeDateModule
 } from '@angular/material';
 import {CountdownComponent} from './countdown/countdown.component';
-import {KycComponent} from './kyc/kyc.component';
-import {MatProgressSpinnerModule, MatCardModule, MatIconModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BrowserModule} from '@angular/platform-browser';
 import {ControlMessagesComponent} from './control-message/control-message.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
@@ -39,12 +34,15 @@ import {FormsModule} from '@angular/forms';
     MatDatepickerModule,
     MatNativeDateModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   declarations: [
     HomeComponent,
     KycComponent,
-    ControlMessagesComponent
+    ControlMessagesComponent,
+    HomeComponent,
+    CountdownComponent,
+    KycComponent,
   ],
   exports: [
     AppRoutingModule,
@@ -63,29 +61,7 @@ import {FormsModule} from '@angular/forms';
   ],
   providers: [
     AuthService,
-  ],
-    imports: [
-        BrowserAnimationsModule,
-        SharedModule,
-        AppRoutingModule,
-        CommonModule,
-        MatProgressSpinnerModule,
-        MatCardModule,
-        MatIconModule,
-        BrowserModule,
-        FormsModule,
-    ],
-    declarations: [
-        HomeComponent,
-        CountdownComponent,
-        KycComponent,
-    ],
-    exports: [
-        AppRoutingModule
-    ],
-    providers: [
-        AuthService
-    ],
+  ]
 })
 export class CoreModule {
 }
