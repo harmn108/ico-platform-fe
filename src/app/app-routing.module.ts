4c904004e267327a2cc4680651c6a1e38672d4e0
haykgrigoryan <haykgrigoryan1989@gmail.com>
2018-02-13 16:44:47 +0400

    add template component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,43 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './core/home/home.component';
+import {KycComponent} from './core/kyc/kyc.component';
+import {TemplateComponent} from './shared/template/template.component';
+import {LanguageGuard} from './shared/guards/language/language.guard';
 
 const routes: Routes = [
     {
-        path: '', pathMatch: 'full', component: HomeComponent
+      path: '',
+      pathMatch: 'full',
+      redirectTo: 'en',
+    },
+    {
+      path: ':language',
+      component: TemplateComponent,
+      canActivate: [LanguageGuard],
+      children: [
+        {
+          path: '',
+          pathMatch: 'full',
+          component: HomeComponent
+        },
+        {
+          path: 'kyc',
+          pathMatch: 'full',
+          component: KycComponent
+        }
+      ]
+    },
+    {
+      path: '**',
+      redirectTo: 'en/page-not-found'
     }
-];
+  ]
+;
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
