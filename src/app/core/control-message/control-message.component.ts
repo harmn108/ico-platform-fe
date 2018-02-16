2ff6bd9b61ae1e3ed7c026fe25d554c03a751773
Sergey Tangyan <sergeytangyan@gmail.com>
2018-02-16 18:43:57 +0400

    added fields and file upload logic

--- src/app/core/control-message/control-message.component.ts
+++ src/app/core/control-message/control-message.component.ts
@@ -5,31 +5,31 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 
 @Component({
-    selector: 'app-control-message',
-    templateUrl: './control-message.component.html'
+  selector: 'app-control-message',
+  templateUrl: './control-message.component.html'
 })
 
 export class ControlMessagesComponent implements OnChanges {
-    @Input() control: FormControl;
-    errorMessage = '';
+  @Input() control: FormControl;
+  errorMessage = '';
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    ngOnChanges(changes: SimpleChanges) {
-        if(changes.control.currentValue && changes.control.currentValue != 'undefined'){
-            changes.control.currentValue.valueChanges.debounceTime(400).subscribe(val => {
-                this.getError();
-            });
-        }
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.control.currentValue && changes.control.currentValue !== 'undefined') {
+      changes.control.currentValue.valueChanges.debounceTime(400).subscribe(val => {
+        this.getError();
+      });
     }
+  }
 
-    getError() {
-        for (const propertyName in this.control.errors) {
-            if (this.control.errors.hasOwnProperty(propertyName) && this.control.dirty) {
-                return this.errorMessage = ValidationService.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
-            }
-        }
-        return this.errorMessage = null;
+  getError() {
+    for (const propertyName in this.control.errors) {
+      if (this.control.errors.hasOwnProperty(propertyName) && this.control.dirty) {
+        return this.errorMessage = ValidationService.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
+      }
     }
+    return this.errorMessage = null;
+  }
 }
--- src/app/core/kyc/kyc.component.ts
+++ src/app/core/kyc/kyc.component.ts
@@ -1,48 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Component, OnInit} from "@angular/core";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {BACKEND_URL} from "../../../environments/parameters";
+import {RequestOptions} from "@angular/http";
 
 @Component({
-  selector: 'app-kyc',
-  templateUrl: './kyc.component.html',
-  styleUrls: ['./kyc.component.scss']
+  selector: "app-kyc",
+  templateUrl: "./kyc.component.html",
+  styleUrls: ["./kyc.component.scss"]
 })
 export class KycComponent implements OnInit {
 
   kycForm = new FormGroup({
-    'fullname': new FormControl('', [Validators.required]),
-    'bday': new FormControl('', [Validators.required]),
-    'imageInput': new FormControl('', [Validators.required]),
-    'wallets': new FormArray([new FormControl('', [Validators.required])]),
+    "fullName": new FormControl("", [Validators.required]),
+    "birthDay": new FormControl("", [Validators.required]),
+    "address": new FormControl("", [Validators.required]),
+    "country": new FormControl("", [Validators.required]),
+    "idScan": new FormControl("", [Validators.required]),
+    "gender": new FormControl("", [Validators.required]),
+    "photo": new FormControl("", [Validators.required]),
+    "zip": new FormControl("", [Validators.required]),
   });
-  public fullname;
-  public bday;
-  public file;
+
   private errorMessages: string;
   private conditionsWarning: string;
+  private uploadMaxSize = 2603875;
+  private uploadTypes = {
+    "photo": ["image/png", "image/jpg", "image/jpeg"],
+    "idScan": ["application/pdf"],
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
-    this.kycForm.valueChanges.subscribe(data => {
+    this.kycForm.valueChanges.subscribe(() => {
 
-        console.log('valid--- ', this.kycForm.valid);
+        console.log("valid--- ", this.kycForm.valid, this.kycForm);
 
-        this.errorMessages = '';
-        this.conditionsWarning = '';
+        this.errorMessages = "";
+        this.conditionsWarning = "";
       },
       err => console.log(err)
     );
 
   }
 
-  get wallets(): FormArray { return this.kycForm.get('wallets') as FormArray; }
+  onFileChange($event, controlName: string) {
+    if ($event.target.files.length > 0) {
+      const file = $event.target.files[0]; // <--- File Object for future use.
+      console.log(file);
+      if (file.size > this.uploadMaxSize) {
+        console.log("asdasd");
+        this.kycForm.controls[controlName].setErrors(["invalid file size"]);
+        return;
+      }
+      if (!this.uploadTypes[controlName].includes(file.type)) {
+        console.log("swwww");
+        this.kycForm.controls[controlName].setErrors(["invalid file type"]);
+        return;
+      }
+      this.kycForm.controls[controlName].setValue(file); // <-- Set Value for Validation
+    }
+  }
 
-  addWallet() { this.wallets.push(new FormControl()); }
-
-  onFileChange($event) {
-    console.log('88888');
-    let file = $event.target.files[0]; // <--- File Object for future use.
-    this.kycForm.controls['imageInput'].setValue(file ? file.name : ''); // <-- Set Value for Validation
+  submitKyc() {
+    const formData: FormData = new FormData();
+    // formData.append('uploadFile', file, file.name);
+    console.log(this.kycForm.getRawValue());
+    // this.http.post(`${BACKEND_URL}/api/v1/user/kyc`, formData,
+    //   {headers: new HttpHeaders({"Content-Type": "multipart/form-data"})});
   }
 
 }
